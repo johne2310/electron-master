17a9b388e6d8828d201f6fa00800c974284e301f
johne2310 <john@day41.com.au>
2020-06-27 13:58:02 +1000

    Initial Commit: Master-Electron Course Files

--- main.js
+++ main.js
@@ -1,108 +1,39 @@
 // Modules
-const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
-const windowStateKeeper = require("electron-window-state");
-const readItem = require("./readItem");
-// const readerJS = require("./renderer/readerJS");
+const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-let readerWindow;
-
-nativeTheme.themeSource = "light";
-
-//listen for new item request
-ipcMain.on("new-item", (e, itemUrl) => {
-  //get new item and send to renderer
-  readItem(itemUrl, item => {
-    e.sender.send("new-item-success", item);
-  });
-});
+let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow() {
-  let state = windowStateKeeper({
-    defaultWidth: 500,
-    defaultHeight: 650
-  });
+function createWindow () {
 
   mainWindow = new BrowserWindow({
-    x: state.x,
-    y: state.y,
-    width: state.width,
-    height: state.height,
-    minWidth: 350,
-    maxWidth: 650,
-    minHeight: 300,
-    webPreferences: { nodeIntegration: true, enableRemoteModule: true }
-  });
-
-  readerWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    minWidth: 350,
-    maxWidth: 2000,
-    maxHeight: 2000,
-    minHeight: 300,
-    show: false,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: false
-    }
-  });
+    width: 1000, height: 800,
+    webPreferences: { nodeIntegration: true }
+  })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile("renderer/main.html");
-  // readerWindow.loadURL("https://electronjs.org");
+  mainWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
-
-  //manage window state
-  state.manage(mainWindow);
+  mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-
-    // if main window closes, then also close readerWindow
-    readerWindow.close();
-    readerWindow = null;
-  });
-
-  //listen for close event and hide window, rather than destroying
-  readerWindow.on("close", e => {
-    e.preventDefault();
-    readerWindow.hide();
-  });
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
 }
 
 // Electron `app` is ready
-app.on("ready", () => {
-  console.log("App is ready");
-  createWindow();
-});
+app.on('ready', createWindow)
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
-});
-
-ipcMain.on("show-window", (event, contentURL, readerJS) => {
-  readerWindow.loadURL(contentURL);
-  readerWindow.show();
-
-  // event.reply("reader-loaded");
-  // readerWindow.webContents.executeJavaScript("alert('hello from ipcMain')");
-  // console.log("readerJS: ", readerJS);
-  readerWindow.webContents
-    .executeJavaScript('{ document.createElement("div") }')
-    .catch(e => {
-      console.log("button error: ", e.message);
-    });
-});
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
